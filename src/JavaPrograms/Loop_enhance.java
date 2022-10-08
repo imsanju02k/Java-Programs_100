334eeacc260bc57608ad5a65d35324a6e7114e6f
imsanju2k <imsanju2k@gmail.com>
2022-10-08 14:57:40 +0530

    java 100 programs

---
+++ src/JavaPrograms/Loop_enhance.java
@@ -0,0 +1,10 @@
+package JavaPrograms;
+
+public class Loop_enhance {
+    public static void main(String[] args) {
+        int prime[]={2,4,6,8};
+        for (int t:prime){
+            System.out.println(t);
+        }
+    }
+}

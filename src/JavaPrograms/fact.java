cf7573d1902182d4bb2d049e521de9aa964eab3d
Sanjay <imsanju2k@gmail.com>
2022-12-04 22:22:01 +0530

    java 100 programs

--- src/JavaPrograms/fact.java
+++ src/JavaPrograms/fact.java
@@ -7,12 +7,13 @@ public class fact {
         if(n==0){
             return 1;
         }
-        return n * factorial(n-1);
+        return n * factorial(n-1) ;
     }
 
     public static void main(String[] args) {
 
-        System.out.println(factorial(4));
+
+        System.out.println( factorial(5));
 
     }
 }
